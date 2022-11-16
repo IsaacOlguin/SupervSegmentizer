54e6d261ca9b139c3ff4dc5875697ec1c784270a
IsaacOlguin <misael.olguin@gmail.com>
2022-11-16 23:48:55 +0100

    First version of login page and paths to dashboard

--- FrontEnd/superv-segm-fe/src/App.js
+++ FrontEnd/superv-segm-fe/src/App.js
@@ -1,9 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom' //Switch was replaced in newer versions by Routes
+import Dashboard from './components/Dashboard/Dashboard';
+import About from './components/About/About';
+import Login from './components/Login/Login';
+
 function App() {
+  const [ token, setToken ] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
-    <div className="App">
+    <div className='wrapper'>
+      <h1>SupervSegmentizer App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+
+  );
+}
+
+export default App;
+
+
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,8 +47,11 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
+    </div>*/
 
-export default App;
+    /*<Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>*/
